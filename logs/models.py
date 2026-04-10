cdb87dbbc7bcb70ed4194caf405ad8aef228ce1a
isaacandresbaleero-hub <isaacandresbaleero@gmail.com>
2026-04-10 15:33:55 +0300

    replace coma with full colon

--- logs/models.py
+++ logs/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class daily_log(models.Model):
+
+    #link the log to user(student)
+    student = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_current_user_id = True,
+        on_delete=models.CASCADE,
+        limit_choices_to={'role' : 'student'}
+    )
+
+
+    date = models.DateField()
+    tasks_performed = models.TextField()
+    lessons_learned = models.TextField()
+    challenges_faced =  models.TextField()
+
+    #supervisor actions
+    is_varified = models.BooleanField(default=False)    
+    supervisor_comments = models.TextField(blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return f" {self.student.username} - {self.date}"
+
+

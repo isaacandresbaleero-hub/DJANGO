8771c00fc2f4ebaff6ab8d5c0a38ebd092dce68f
isaacandresbaleero-hub <isaacandresbaleero@gmail.com>
2026-04-10 16:07:17 +0300

    enhance daily logs view

--- logs/admin.py
+++ logs/admin.py
@@ -4,4 +4,8 @@ from .models import daily_log
 
 
 # Register your models here.
-admin.site.register(daily_log)
+@admin.site.register(daily_log)
+class daily_LogAdmin(admin.ModelAdmin):
+    list_display = ('student' , 'date')
+    list_filter = ('date', 'student')
+    search_fields = ('student__username')

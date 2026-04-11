0c2c992137d28fef18d7121631247501b4e41da5
isaacandresbaleero-hub <isaacandresbaleero@gmail.com>
2026-04-11 19:50:54 +0300

    error correction

--- placements/admin.py
+++ placements/admin.py
@@ -4,7 +4,7 @@ from .models import internship_placement
 @admin.register(internship_placement)
 class InternshipPlacementAdmin(admin.ModelAdmin):
     list_display = ('students', 'organization_name', 'start_date', 'end_date', 'is_active')
-    search_fields = ('students__username', 'organization_name')
+    search_fields = ('organization_name', 'students__username')
 
 
 

7730bd98a242dcaca9136139cda2ddcbe18bfc98
isaacandresbaleero-hub <isaacandresbaleero@gmail.com>
2026-04-11 18:32:27 +0300

    defining placement model

--- placements/models.py
+++ placements/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
+class internship_placement(models.model)
+    students = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASADE,
+        limit_choices_to= {'role': 'student'}
+    )

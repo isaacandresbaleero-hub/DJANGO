e72d8eb17a8286ab10257dd5eefa0193ab9e682e
isaacandresbaleero-hub <isaacandresbaleero@gmail.com>
2026-04-11 22:18:10 +0300

    impoert models to admin

--- Evaluations/admin.py
+++ Evaluations/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import EvaluationsCriteria, Evaluations
 
-# Register your models here.
+
+@admin.register(EvaluationsCriteria)
+class EvaluationsCriteriaAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Evaluations)
+class EvaluationsAdmin(admin.ModelAdmin):
+    list_display = ('placement', 'criteria', 'score', 'date_Evatuated')
+    list_filter = ('criteria', 'placement__organization_name')

d5abfc03b459ab213fe1d727cba5ec2a7246e91a
isaacandresbaleero-hub <isaacandresbaleero@gmail.com>
2026-04-11 19:46:53 +0300

    update Admin.py in placement

--- placements/admin.py
+++ placements/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import internship_placement
+
+@admin.register(internship_placement)
+class InternshipPlacementAdmin(admin.ModelAdmin):
+    list_display = ('students', 'organization_name', 'start_date', 'end_date', 'is_active')
+    search_fields = ('students__username', 'organization_name')
+
+
 
 # Register your models here.

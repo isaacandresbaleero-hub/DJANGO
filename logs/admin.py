03350f7a57c40273a15a919765ec13fee2eae299
isaacandresbaleero-hub <isaacandresbaleero@gmail.com>
2026-04-11 17:40:15 +0300

    updating students drop down in settings.py

--- logs/admin.py
+++ logs/admin.py
@@ -9,3 +9,10 @@ class daily_LogAdmin(admin.ModelAdmin):
     list_display = ('student' , 'date')
     list_filter = ('date', 'student')
     search_fields = ('student__username',)
+
+
+
+def save_model(self, request, obj, form, change):
+    if not change:  # Only set the student field when creating a new log
+        obj.student = request.user
+    super().save_model(request, obj, form, change)  
